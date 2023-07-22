d7b2b0149d0605063f80ebd6c1f81e3ab9741de7
go2Cshop <shell800808@gmail.com>
2023-07-22 23:49:54 +0800

    feat: add seeders: categorySeeder and recordSeeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,12 +1,49 @@
-const db = require('../../config/mongoose')
+
 const Record = require('../record')
 const User = require('../user')
 const Category = require('../category')
-const data = require('./data.json').data
 
+const db = require('../../config/mongoose')
 
-const SEED_USER = data.SEED_USER
-const SEED_RECORD = data.SEED_RECORD
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
+
+const SEED_RECORD = [
+
+  {
+    "name": "午餐",
+    "date": "2019/4/23",
+    "amount": "60",
+    "category_name": "餐飲食品"
+  },
+  {
+    "name": "晚餐",
+    "date": "2019/4/23",
+    "amount": "60",
+    "category_name": "餐飲食品"
+  },
+  {
+    "name": "捷運",
+    "date": "2019/4/23",
+    "amount": "120",
+    "category_name": "交通出行"
+  },
+  {
+    "name": "電影:驚奇隊長",
+    "date": "2019/4/23",
+    "amount": "60",
+    "category_name": "休閒娛樂"
+  },
+  {
+    "name": "租金",
+    "date": "2019/4/1",
+    "amount": "25000",
+    "category_name": "家居物業"
+  }
+]
 
 db.once('open', () => {
   Promise.all(
@@ -35,4 +72,4 @@ db.once('open', () => {
           })
         })
     )
-}) 
+})
